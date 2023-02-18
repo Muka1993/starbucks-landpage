52032e8b1065217c21079a46ee320fee10c884df
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2023-02-18 16:00:02 -0300

    Adicionando botão dark mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,19 @@
 
+// FUNÇÃO QUE AO CLICAR NO STARBUCKS TROCA OS COPOS
+
 function imgSlider(mudando){
     document.querySelector('.starbucks').src = mudando;
 }
+
+// FUNÇÃO QUE MUDA A COR DO CIRCULO EM VOLTA DOS STARBUCKS
 
 function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+
+// FUNÇAO PARA MENU HAMBURGUER
 
 var menu = document.querySelector('.menu');
 
@@ -18,6 +25,7 @@ function clickMenu(){
     }
 }
 
+// FUNÇÃO QUE FORÇA A MUDANÇA DO MENU HAMBURGUER PARA BLOCK  
 
 function telaTamanho() {
     if(window.innerWidth >= '992'){
@@ -27,5 +35,18 @@ function telaTamanho() {
    }
  }
 
+// FUNÇÃO DARK MODE
 
+const btnDarkMode = document.querySelector('.dark_mode_svg');
+
+btnDarkMode.addEventListener("click", (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+    e.target.style.color = 'black'
+    }else{
+        html.classList.add('dark')
+    e.target.style.color = 'white'    
+    }
+})
 
